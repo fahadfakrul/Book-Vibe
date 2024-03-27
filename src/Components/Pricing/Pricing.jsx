8891710388bb45c3056f0db11abf190e5756a764
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-03-27 18:03:46 +0600

    extra pricing route created sucessfully

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,10 +1,10 @@
 
 const Pricing = () => {
     return (
-        <section className="container mx-auto py-20 dark:bg-gray-100 dark:text-gray-800">
+        <section className="container mx-auto py-20  dark:text-black">
 	<div className="container px-4 mx-auto">
 		<div className="max-w-2xl mx-auto mb-16 text-center">
-			<span className="font-bold tracking-wider uppercase dark:text-violet-600">Pricing</span>
+			<span className="font-bold tracking-wider uppercase dark:text-[#23BE0A]">Pricing</span>
 			<h2 className="text-4xl font-bold lg:text-5xl">Choose your best plan</h2>
 		</div>
 		<div className="flex flex-wrap items-stretch -mx-4">
@@ -14,110 +14,115 @@ const Pricing = () => {
 						<h4 className="text-2xl font-bold">Beginner</h4>
 						<span className="text-6xl font-bold">Free</span>
 					</div>
-					<p className="mt-3 leading-relaxed dark:text-gray-600">Etiam ac convallis enim, eget euismod dolor.</p>
+					<p className="mt-3 leading-relaxed dark:text-gray-600">Access basic features of the website for free.</p>
 					<ul className="flex-1 mb-6 dark:text-gray-600">
 						<li className="flex mb-2 space-x-2">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-violet-600">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-[#23BE0A]">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Aenean quis</span>
+							<span>Browse book club listings.</span>
+
+
 						</li>
 						<li className="flex mb-2 space-x-2">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-violet-600">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-[#23BE0A]">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Morbi semper</span>
+							<span>View book club details and descriptions.</span>
 						</li>
 						<li className="flex mb-2 space-x-2">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-violet-600">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-[#23BE0A]">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Tristique enim nec</span>
+							<span>Contact book clubs via provided email addresses.</span>
 						</li>
 					</ul>
-					<button type="button" className="inline-block px-5 py-3 font-semibold tracking-wider text-center rounded dark:bg-violet-600 dark:text-gray-50">Get Started</button>
+					<button type="button" className="btn inline-block px-5 py-3 font-semibold tracking-wider text-center rounded dark:bg-[#23BE0A] dark:text-gray-50">Get Started</button>
 				</div>
 			</div>
 			<div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
-				<div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 dark:bg-violet-600 dark:text-gray-50">
+				<div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 dark:bg-[#23BE0A] dark:text-gray-50">
 					<div className="space-y-2">
-						<h4 className="text-2xl font-bold">Pro</h4>
-						<span className="text-6xl font-bold">$24
+						<h4 className="text-2xl font-bold">Standard</h4>
+						<span className="text-6xl font-bold">$5
 							<span className="text-sm tracking-wide">/month</span>
 						</span>
 					</div>
-					<p className="leading-relaxed">Morbi cursus ut sapien sit amet consectetur.</p>
+					<p className="leading-relaxed">Unlock additional features and benefits with the Standard Tier subscription.</p>
 					<ul className="flex-1 space-y-2">
 						<li className="flex items-center space-x-2">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Everything in Free</span>
+							<span>All features included in the Free Tier.</span>
+
+
+
 						</li>
 						<li className="flex items-center space-x-2">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Phasellus tellus</span>
+							<span>Ability to join discussions and participate in book club activities.</span>
 						</li>
 						<li className="flex items-center space-x-2">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Praesent faucibus</span>
+							<span>Access to exclusive content, such as author interviews or advanced reading guides.</span>
 						</li>
 						<li className="flex items-center space-x-2">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Aenean et lectus blandit</span>
+							<span>Personalized book recommendations based on reading preferences.</span>
 						</li>
 					</ul>
-					<a rel="noopener noreferrer" href="#" className="inline-block w-full px-5 py-3 font-bold tracking-wider text-center rounded dark:bg-gray-100 dark:text-violet-600">Get Started</a>
+					<button type="button" className="btn inline-block px-5 py-3 font-semibold tracking-wider text-center rounded dark:bg-[#23BE0A] dark:text-gray-50">Get Started</button>
 				</div>
 			</div>
 			<div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
 				<div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 dark:bg-gray-50">
 					<div className="space-y-2">
-						<h4 className="text-2xl font-bold">Team</h4>
-						<span className="text-6xl font-bold">$72
+						<h4 className="text-2xl font-bold">Premium</h4>
+						<span className="text-6xl font-bold">$10
 							<span className="text-sm tracking-wide">/month</span>
 						</span>
 					</div>
-					<p className="leading-relaxed dark:text-gray-600">Phasellus ultrices bibendum nibh in vehicula.</p>
+					<p className="leading-relaxed dark:text-gray-600">Get the ultimate book club experience with the Premium Tier subscription.</p>
 					<ul className="space-y-2 dark:text-gray-600">
 						<li className="flex items-start space-x-2">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-violet-600">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-[#23BE0A]">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Everything in Pro</span>
+							<span>All features included in the Standard Tier.</span>
 						</li>
 						<li className="flex items-start space-x-2">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-violet-600">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-[#23BE0A]">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Fusce sem ligula</span>
+							<span>Priority access to limited-capacity book club events or author meet-and-greets.</span>
 						</li>
 						<li className="flex items-start space-x-2">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-violet-600">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-[#23BE0A]">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Curabitur dictum</span>
+							<span>Discounts on book purchases or merchandise from partnered bookstores.</span>
 						</li>
 						<li className="flex items-start space-x-2">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-violet-600">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-[#23BE0A]">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
-							<span>Duis odio eros</span>
+							<span>Ad-free browsing experience.</span>
 						</li>
 						<li className="flex items-start space-x-2">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-violet-600">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 dark:text-[#23BE0A]">
 								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 							</svg>
 							<span>Vivamus ut lectus ex</span>
 						</li>
 					</ul>
-					<a rel="noopener noreferrer" href="#" className="inline-block w-full px-5 py-3 font-semibold tracking-wider text-center rounded dark:bg-violet-600 dark:text-gray-50">Get Started</a>
+					<button type="button" className="btn inline-block px-5 py-3 font-semibold tracking-wider text-center rounded dark:bg-[#23BE0A] dark:text-gray-50">Get Started</button>
 				</div>
 			</div>
 		</div>
